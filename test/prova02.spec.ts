a06273e7df72d13d2321497795bc8d3ff34434c1
Gabrielle <gabriellecoelhoytb@gmail.com>
2025-09-29 19:33:06 -0300

    commit prova 29-09

--- test/prova02.spec.ts
+++ test/prova02.spec.ts
@@ -14,21 +14,139 @@ describe('Dummy Json Api', () => {
   p.request.setDefaultTimeout(90000);
 
   beforeAll(async () => {});
+  
+  describe('Recipes', () => {
+    it('Get Recipes', async () => {
+      await p
+        .spec()
+        .get(`${baseUrl}/recipes`)
+        .expectStatus(StatusCodes.OK);
+    });
 
-  it('Get Recipes', async () => {
-    const response = await p
-      .spec()
-      .get(`${baseUrl}/recipes`)
-      .expectStatus(StatusCodes.OK);
-      //.inspect();
+    it('Get Specific Recipe', async () => {
+      await p
+        .spec()
+        .get(`${baseUrl}/recipes/search?q=Margherita`)
+        .expectStatus(StatusCodes.OK);
+    });
   });
+  
+  describe('Posts', () => {
+    it('Get Posts', async () => {
+      await p
+        .spec()
+        .get(`${baseUrl}/posts`)
+        .expectStatus(StatusCodes.OK);
+    });
+    it('Get Specific Post', async () => {
+      await p
+        .spec()
+        .get(`${baseUrl}/posts/250`)
+        .expectStatus(StatusCodes.OK)
+        .inspect();
+    });
+    it('Add a new post created', async () => {
+      await p
+        .spec()
+        .post(`${baseUrl}/posts/add`)
+        .withHeaders('Content-Type', 'application/json')
+        .withJson({
+          title: 'I am in love with someone.',
+          userId: 5
+        })
+        .expectStatus(StatusCodes.CREATED)
+        .expectJsonLike({
+          title: 'I am in love with someone.',
+          userId: 5
+        });
+    });
 
-  it('Get Specific Recipe', async () => {
-    const response = await p
-      .spec()
-      .get(`${baseUrl}/recipes/search?q=Margherita`)
-      .expectStatus(StatusCodes.OK)
-      .inspect();
+    it('Update post success', async () => {
+      await p
+        .spec()
+        .put(`${baseUrl}/posts/250`)
+        .withHeaders('Content-Type', 'application/json')
+        .withJson({
+          title: 'The peaceful village.'
+        })
+        .expectStatus(StatusCodes.OK)
+        .expectJsonLike({
+          id: 250,
+          title: 'The peaceful village.'
+        })
+        .inspect();
+    });
+    it('Update post failed', async () => {
+      await p
+        .spec()
+        .put(`${baseUrl}/posts/252`)
+        .withHeaders('Content-Type', 'application/json')
+        .withJson({
+          title: 'The peaceful village.'
+        })
+        .expectStatus(StatusCodes.NOT_FOUND);
+    });
+  });
+  describe('Products', () => {
+    it('Search Product succes', async () => {
+      await p
+        .spec()
+        .get(`${baseUrl}/products/search?q=phone`)
+        .expectStatus(StatusCodes.OK)
+        .inspect();
+    });
+    it('Search Product not found', async () => {
+      await p
+        .spec()
+        .get(`${baseUrl}/products/search?q=phhone`)
+        .expectJsonLike({
+          "products": []
+        });
+    });
+    it('should create a new product', async () => {
+      await p
+        .spec()
+        .post(`${baseUrl}/products/add`)
+        .withHeaders('Content-Type', 'application/json')
+        .withJson({
+          title: 'BMW Pencil',
+          price: 3.99,
+          brand: 'BMW',
+          category: 'stationery'
+        })
+        .expectStatus(StatusCodes.CREATED)
+        .expectJsonLike({
+          title: 'BMW Pencil',
+          brand: 'BMW'
+        })
+        .inspect(); 
+    });
+    it('Update an existing product', async () => {
+      await p
+        .spec()
+        .put('https://dummyjson.com/products/1')
+        .withJson({
+          title: 'Updated BMW Pencil',
+          price: 4.99
+        })
+        .expectStatus(200)
+        .expectJsonLike({
+          id: 1,
+          title: 'Updated BMW Pencil',
+          price: 4.99
+        })
+        .inspect();
+    });
+    it('Delete an existing product', async () => {
+      await p
+        .spec()
+        .delete('https://dummyjson.com/products/1')
+        .expectStatus(200)
+        .expectJsonLike({
+          id: 1
+        })
+        .inspect();
+    });    
   });
 
 afterAll(() => p.reporter.end());
